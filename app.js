445902f2038202bc5ec541d3f9a31b34cd67f785
florian-barthelemy <florian-barthelemy@hotmail.com>
2024-03-19 13:49:12 +0100

    feat :setup ok

---
+++ app.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+//création d'une constante app qui va contenir l'application express.js
+const app = express();
+
+//la variable port est utilisée pour définir le port sur lequel le serveur va écouter les requêtes le choix du port est arbitraire.
+const port = 3000;
+
+//Mise en place d'un console log pour indiquer que le serveur est en écoute sur le port que nous avons défini.
+app.listen(port, () => console.log(`En écoute sur le port ${port}!`));
+
+//création de l'application express.js
+app.use(express.json());
